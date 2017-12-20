af4144bae22a13a4ec64e337184e2a7b28cfaef4
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-12-20 22:46:58 +0300

    add pattern for new package

---
+++ src/main/java/corp/siendev/filesystem/FileStack.java
@@ -0,0 +1,15 @@
+package corp.siendev.filesystem;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileStack {
+
+    public void fileInfo(String fileName) throws IOException {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            throw new FileNotFoundException(String.format("File %s not found", file));
+        }
+    }
+}
