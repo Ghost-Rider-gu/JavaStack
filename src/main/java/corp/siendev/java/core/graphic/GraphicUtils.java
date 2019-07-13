a4b4b75bfd3e48657857d5df90fc88cfe05e12c9
Ghost-Rider <ghostrider.gu@gmail.com>
2019-07-13 21:18:57 +0300

    added GraphicUtils class

---
+++ src/main/java/corp/siendev/java/core/graphic/GraphicUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019 SienDev company. All rights reserved.
+ *
+ * This is a free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ */
+
+package corp.siendev.java.core.graphic;
+
+import javax.imageio.ImageIO;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class GraphicUtils {
+
+    public void takeScreenShot(String screenShot) throws Exception {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle screenRectangle = new Rectangle(screenSize);
+        Robot robot = new Robot();
+        BufferedImage image = robot.createScreenCapture(screenRectangle);
+        ImageIO.write(image, "png", new File(screenShot));
+    }
+}
