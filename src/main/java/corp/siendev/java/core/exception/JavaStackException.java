1050b966e9efaafd2f02a6a7f2f7068c50c49196
Ghost-Rider <ghostrider.gu@gmail.com>
2019-07-20 20:55:24 +0300

    added Exception class for the project

---
+++ src/main/java/corp/siendev/java/core/exception/JavaStackException.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019 SienDev company. All rights reserved.
+ *
+ * This is a free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ */
+
+package corp.siendev.java.core.exception;
+
+public class JavaStackException extends Exception {
+
+    public JavaStackException() {
+        super();
+    }
+
+    public JavaStackException(String message) {
+        super(message);
+    }
+}
