25c20c0fdbc9fcc716ed07bbceb998f0f2b3e3d3
Ghost-Rider <ghostrider.gu@gmail.com>
2019-07-23 19:00:09 +0300

    added Pattern class

---
+++ src/main/java/corp/siendev/java/core/pattern/PatternStuff.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019 SienDev company. All rights reserved.
+ *
+ * This is a free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ */
+
+package corp.siendev.java.core.pattern;
+
+import java.util.regex.Pattern;
+
+public class PatternStuff {
+
+    private static final Pattern IP_PATTERN = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+    public static String pattern = IP_PATTERN.pattern();
+
+    public static void main(String[] args) {
+        String ip = "123.56.89.09";
+        System.out.println(ip.matches(pattern));
+    }
+}
